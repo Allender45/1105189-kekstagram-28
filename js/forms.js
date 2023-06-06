35c9f077be4b409260dee4a937f4977b2f6363a7
Максим Бервинов <crash-em@mail.ru>
2023-06-06 15:56:02 +0500

    Поработал с кнопками фильтров. Не получилось устранить "дребезг". Переписал блок фильтров-эффектов. Добавил делегирование. Добавил комментарии к большому фото. Пока без пагинации.

--- js/forms.js
+++ js/forms.js
@@ -49,7 +49,7 @@ uploadImgForm.addEventListener('input', (evt) => {
         hideModal();
       }
     }
-  }, {once:true});
+  }, {once: true});
 
   const preview = uploadImgForm.querySelector('.img-upload__preview img');
   const file = uploadImgForm.querySelector('#upload-file').files[0];
@@ -138,99 +138,71 @@ noUiSlider.create(slider, {
 
 sliderContainer.classList.add('hidden');
 
-document.querySelector('.effects').addEventListener('change', (evt) => {
-  if (evt.target.classList.contains('effects__preview--none')) {
-    sliderContainer.classList.add('hidden');
-    imgPreview.removeAttribute('style');
+const FILTER_EFFECTS = {
+  'effects__preview--none': () => imgPreview.removeAttribute('style'),
+  'effects__preview--chrome': {
+    'options': {
+      range: {min: 0, max: 1},
+      step: 0.1,
+      start: 1
+    },
+    'filter': 'grayscale',
+    'piece': ''
+  },
+  'effects__preview--sepia': {
+    'options': {
+      range: {min: 0, max: 1},
+      step: 0.1,
+      start: 1
+    },
+    'filter': 'sepia',
+    'piece': ''
+  },
+  'effects__preview--marvin': {
+    'options': {
+      range: {min: 0, max: 100},
+      step: 1,
+      start: 100
+    },
+    'filter': 'invert',
+    'piece': '%'
+  },
+  'effects__preview--phobos': {
+    'options': {
+      range: {min: 0, max: 3},
+      step: 0.1,
+      start: 3
+    },
+    'filter': 'blur',
+    'piece': 'px'
+  },
+  'effects__preview--heat': {
+    'options': {
+      range: {min: 1, max: 3},
+      step: 0.1,
+      start: 3
+    },
+    'filter': 'brightness',
+    'piece': ''
   }
+};
 
-  if (evt.target.classList.contains('effects__preview--chrome')) {
-    slider.noUiSlider.updateOptions(
-      {
-        range: {
-          min: 0,
-          max: 1
-        },
-        step: 0.1,
-        start: 1
-      }
-    );
-    imgPreview.style.filter = `grayscale(${slider.noUiSlider.get()})`;
-    slider.noUiSlider.on('slide', () => {
-      imgPreview.style.filter = `grayscale(${slider.noUiSlider.get()})`;
-    });
-    sliderContainer.classList.remove('hidden');
-  }
+const addFilterEffects = (effect) => {
+  slider.noUiSlider.updateOptions(effect['options']);
+  imgPreview.style.filter = `${effect['filter']}(${slider.noUiSlider.get()})`;
+  slider.noUiSlider.on('slide', () => {
+    imgPreview.style.filter = `${effect['filter']}(${slider.noUiSlider.get()}${effect['piece']})`;
+  });
+  sliderContainer.classList.remove('hidden');
+};
 
-  if (evt.target.classList.contains('effects__preview--sepia')) {
-    slider.noUiSlider.updateOptions(
-      {
-        range: {
-          min: 0,
-          max: 1
-        },
-        step: 0.1,
-        start: 1
-      }
-    );
-    imgPreview.style.filter = `sepia(${slider.noUiSlider.get()})`;
-    slider.noUiSlider.on('slide', () => {
-      imgPreview.style.filter = `sepia(${slider.noUiSlider.get()})`;
-    });
-    sliderContainer.classList.remove('hidden');
-  }
-
-  if (evt.target.classList.contains('effects__preview--marvin')) {
-    slider.noUiSlider.updateOptions(
-      {
-        range: {
-          min: 0,
-          max: 100
-        },
-        step: 1,
-        start: 100
-      }
-    );
-    imgPreview.style.filter = `invert(${slider.noUiSlider.get()}%)`;
-    slider.noUiSlider.on('slide', () => {
-      imgPreview.style.filter = `invert(${slider.noUiSlider.get()}%)`;
-    });
-    sliderContainer.classList.remove('hidden');
-  }
-
-  if (evt.target.classList.contains('effects__preview--phobos')) {
-    slider.noUiSlider.updateOptions(
-      {
-        range: {
-          min: 0,
-          max: 3
-        },
-        step: 0.1,
-        start: 3
-      }
-    );
-    imgPreview.style.filter = `blur(${slider.noUiSlider.get()}px)`;
-    slider.noUiSlider.on('slide', () => {
-      imgPreview.style.filter = `blur(${slider.noUiSlider.get()}px)`;
-    });
-    sliderContainer.classList.remove('hidden');
-  }
-
-  if (evt.target.classList.contains('effects__preview--heat')) {
-    slider.noUiSlider.updateOptions(
-      {
-        range: {
-          min: 1,
-          max: 3
-        },
-        step: 0.1,
-        start: 3
-      }
-    );
-    imgPreview.style.filter = `brightness(${slider.noUiSlider.get()})`;
-    slider.noUiSlider.on('slide', () => {
-      imgPreview.style.filter = `brightness(${slider.noUiSlider.get()})`;
-    });
-    sliderContainer.classList.remove('hidden');
+document.querySelector('.effects__list').addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('effects__radio')) {
+    if (evt.target.classList[2] === 'effects__preview--none') {
+      sliderContainer.classList.add('hidden');
+      imgPreview.removeAttribute('style');
+    } else {
+      addFilterEffects(FILTER_EFFECTS[evt.target.classList[2]]);
+    }
   }
 });
